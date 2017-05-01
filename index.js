2ff8870353635c881f6e7a92775ab92cf8f89622
allisonlynnbasore14 <allison.basore@students.olin.edu>
2017-05-01 13:46:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('templates/Welcome');
+  response.render('templates/Profile');
 });
 
 app.listen(app.get('port'), function() {
