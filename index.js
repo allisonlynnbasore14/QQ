63df3025c04920dd98f1bb84ae83e15a8089114a
AllisonBasore <allison.basore@students.olin.edu>
2017-05-01 18:18:39 -0400

    "adding another page I hope"

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/', function(request, response) {
   response.render('templates/Welcome');
 });
 
+app.get('/cool', function(request, response) {
+  response.render('templates/Welcome');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
