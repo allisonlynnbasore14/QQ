92e848a5af8615e5e28be353378af1388cf6c136
AllisonBasore <allison.basore@students.olin.edu>
2017-05-12 18:05:59 -0400

    "fixxed all changes"

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var home = function(req, res){
-  res.render("home", {"classes": [
-  {name:"QEA",teacher:"Rebecca"},
-  {name:"Products and Markets",teacher:"Lawrence"},
-  {name: "SoftDes", teacher: "Oliver"}]
-});
-};
-
-module.exports.home = home;
