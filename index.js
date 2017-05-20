47fd0b50267928e906b46c63b734efa9329d8582
AllisonBasore <allison.basore@students.olin.edu>
2017-05-20 16:11:43 -0400

    "further took out real login for test"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 // Configure Stormpath.
 app.use(stormpath.init(app, {
